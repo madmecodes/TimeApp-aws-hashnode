8569be52560ae8e42c06a80337fb8cffea360315
ayush-gupta-dev <ayushguptadev1@gmail.com>
2023-07-30 00:35:01 +0530

    disabled Splashscreen  /MainActivity.java clash with auth

--- android/app/src/main/java/com/timemanager2/MainActivity.java
+++ android/app/src/main/java/com/timemanager2/MainActivity.java
@@ -9,7 +9,7 @@ import org.devio.rn.splashscreen.SplashScreen; // here
 public class MainActivity extends ReactActivity {
    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
+       // SplashScreen.show(this);  // here
         super.onCreate(savedInstanceState);
     }
   /**
